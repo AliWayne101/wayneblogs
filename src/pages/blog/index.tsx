3b0a5934b933c313efdcda23d1bc02a04fe843e6
AliWayne101 <alimalikwayne@gmail.com>
2023-05-02 20:33:24 +0500

    Initial Commit

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,30 @@
+import Hero from '@/sections/Hero'
+import MainBody from '@/sections/MainBody'
+import Navbar from '@/sections/Navbar'
+import Head from 'next/head'
+import React, { useEffect } from 'react'
+import Search from '@/components/Search';
+
+const Index = () => {
+    useEffect(() => {
+        window.addEventListener('scroll', ()=> {
+            if (window.pageYOffset >= window.innerHeight) console.log('page reached');
+        })
+    }, []);
+
+    return (
+        <>
+            <Head>
+                <title>Blog - Wayne Blogs</title>
+            </Head>
+            <Navbar />
+            <main>
+                <Hero />
+                <Search />
+                <MainBody infiniteData={true} />
+            </main>
+        </>
+    )
+}
+
+export default Index
