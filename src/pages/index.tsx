b78c3f0e5d2f6733219dd4276160899fb0c9444a
AliWayne101 <alimalikwayne@gmail.com>
2023-05-04 11:55:35 +0500

    google site verification

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ function Index() {
         <meta name="description" content='Discover a world of ideas, insights, and inspiration with our diverse range of expertly crafted content. From the latest trends to timeless classics, our blog has something for everyone. Join the conversation today and let us guide you on your journey of knowledge and discovery.' />
         <meta name="author" content='Ali Wains' />
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta name="google-site-verification" content="b7D9w3jLmQYeXyUi9jsyf7NsdHZiOGZkCQpfQ3HBb5o" />
       </Head>
       <Navbar />
       <main>
